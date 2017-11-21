cba3080627ff37f0c1eb57ac3635d67a57d0265e
Snap IT Solutions LLC <ram@snapit.solutions>
2017-11-21 14:53:45 -0600

    oosd second class push demo

---
+++ src/printingStatements/Hello.java
@@ -0,0 +1,10 @@
+package printingStatements;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Ram");
+		System.out.println("I like to eat");
+	}
+
+}

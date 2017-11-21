0775de3d7503e2f51e0a93ade1cb0b320435dd7c
Snap IT Solutions LLC <ram@snapit.solutions>
2017-11-21 15:11:09 -0600

    added a new line

--- src/printingStatements/Hello.java
+++ src/printingStatements/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("My name is Ram");
 		System.out.println("I like to eat");
+		System.out.println("I also like to drink water");
 	}
 
 }

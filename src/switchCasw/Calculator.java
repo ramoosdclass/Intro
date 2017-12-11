7ca8b5e7fdc46a4cb4a40394ce457b37e62c2791
Snap IT Solutions LLC <ram@snapit.solutions>
2017-12-11 13:21:24 -0600

    calculator program with switch case

---
+++ src/switchCasw/Calculator.java
@@ -0,0 +1,50 @@
+package switchCasw;
+
+import java.util.Scanner;
+
+public class Calculator {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Please enter number 1: ");
+		int num1 = new Scanner(System.in).nextInt();
+		
+		System.out.println("Please enter number 2: ");
+		int num2 = new Scanner(System.in).nextInt();
+		
+		System.out.println("Please enter an operation to be performed: ");
+		String operator = new Scanner(System.in).nextLine();
+		
+		Calculate(num1, num2, operator);
+		
+	}
+
+
+
+public static void Calculate(int num1, int num2, String operation) {
+	int result;
+	switch(operation) {
+	case "+":
+		result = num1+ num2;
+		System.out.println("The sum is " + result);
+		break;
+	case "-":
+		result = num1 - num2;
+		System.out.println("The difference is " + result);
+		break;
+	case "*":
+		result = num1 * num2;
+		System.out.println("The product is " + result);
+		break;
+	case "/":
+		result = num1 / num2;
+		System.out.println("The quoitent is " + result);
+		break;
+	default:
+		System.out.println("Please enter a valid operator" 
+	+ "Example: +, - , * , /");
+		
+	}
+}
+	
+}
